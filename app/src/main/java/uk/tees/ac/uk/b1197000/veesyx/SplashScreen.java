5c0032205b70769f1ca464118831b35effdb8ddb
David Egbebunmi <david.egbebunmi@posshop-ng.com>
2023-02-21 00:25:35 +0000

    splash screen added

---
+++ app/src/main/java/uk/tees/ac/uk/b1197000/veesyx/SplashScreen.java
@@ -0,0 +1,14 @@
+package uk.tees.ac.uk.b1197000.veesyx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
