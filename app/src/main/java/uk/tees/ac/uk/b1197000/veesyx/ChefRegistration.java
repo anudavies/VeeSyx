070d62a12968a857a1f5a9127e1372b1cc1bac5d
David Egbebunmi <david.egbebunmi@posshop-ng.com>
2023-03-07 13:22:46 +0000

    Different users login and registration page added

---
+++ app/src/main/java/uk/tees/ac/uk/b1197000/veesyx/ChefRegistration.java
@@ -0,0 +1,14 @@
+package uk.tees.ac.uk.b1197000.veesyx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChefRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_registration);
+    }
+}
