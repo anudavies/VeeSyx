94b7fc129fcdcb2508444363c0dc483f66d95c97
David Egbebunmi <anudavies@gmail.com>
2023-04-08 14:32:12 +0100

    Customer Bottom navigation design completed

--- app/src/main/java/uk/tees/ac/uk/b1197000/veesyx/CustomerFoodPanel_BottomNavigation.java
+++ app/src/main/java/uk/tees/ac/uk/b1197000/veesyx/CustomerFoodPanel_BottomNavigation.java
@@ -1,14 +1,68 @@
 package uk.tees.ac.uk.b1197000.veesyx;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class CustomerFoodPanel_BottomNavigation extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import uk.tees.ac.uk.b1197000.veesyx.customerFoodPanel.CustomerCartFragmnet;
+import uk.tees.ac.uk.b1197000.veesyx.customerFoodPanel.CustomerHomeFragment;
+import uk.tees.ac.uk.b1197000.veesyx.customerFoodPanel.CustomerOrdersFragment;
+import uk.tees.ac.uk.b1197000.veesyx.customerFoodPanel.CustomerProfileFragment;
+
+public class CustomerFoodPanel_BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_food_panel_bottom_navigation);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.cust_Home:
+                fragment=new CustomerHomeFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.cart:
+                fragment=new CustomerCartFragmnet();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.cust_profile:
+                fragment=new CustomerProfileFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.Cust_order:
+                fragment=new CustomerOrdersFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.cart:
+                fragment=new CustomerCartFragmnet();
+                break;
+        }
+        return loadcheffragment(fragment);
+
+    }
+
+    private boolean loadcheffragment(Fragment fragment) {
+
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            return true;
+        }
+        return false;
     }
 }
