5fae55dc20a6cfe5480b6ee3f8bb079ac2eebbdb
David Egbebunmi <anudavies@gmail.com>
2023-04-30 22:04:48 +0100

    Chef Pending Order and Delivery Pending Order Completed

--- app/src/main/java/uk/tees/ac/uk/b1197000/veesyx/deliveryFoodPanel/DeliveryPendingOrderFragment.java
+++ app/src/main/java/uk/tees/ac/uk/b1197000/veesyx/deliveryFoodPanel/DeliveryPendingOrderFragment.java
@@ -12,21 +12,52 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import uk.tees.ac.uk.b1197000.veesyx.MainMenu;
+import uk.tees.ac.uk.b1197000.veesyx.OrdersModel;
 import uk.tees.ac.uk.b1197000.veesyx.R;
+import uk.tees.ac.uk.b1197000.veesyx.customerFoodPanel.OrderListAdapter;
 
 
-public class DeliveryPendingOrderFragment extends Fragment {
-
+public class DeliveryPendingOrderFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener{
+    RecyclerView recyclerView;
+    private List<OrdersModel> ordersList;
+    private OrderListAdapter adapter;
+    DatabaseReference data,databaseReference;
+    SwipeRefreshLayout swipeRefreshLayout;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_deliverypendingorder, null);
         getActivity().setTitle("Pending Orders");
         setHasOptionsMenu(true);
+        recyclerView = v.findViewById(R.id.Recycle_menu);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        ordersList = new ArrayList<>();
+        swipeRefreshLayout = (SwipeRefreshLayout)v.findViewById(R.id.swipelayout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimaryDark,R.color.Red);
+
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+            customerOrders();
+            }
+        });
         return v;
     }
     @Override
@@ -50,5 +81,36 @@ public class DeliveryPendingOrderFragment extends Fragment {
         Intent intent = new Intent(getActivity(), MainMenu.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
+    }
+    public void customerOrders(){
+        swipeRefreshLayout.setRefreshing(true);
+        String  userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Orders");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ordersList.clear();
+                for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                    for(DataSnapshot snapshot2 : snapshot1.getChildren()){
+                        OrdersModel orders = snapshot2.getValue(OrdersModel.class);
+                        ordersList.add(orders);
+                    }
+
+
+                }
+                adapter = new OrderListAdapter(getContext(),ordersList,null);
+                recyclerView.setAdapter(adapter);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+    @Override
+    public void onRefresh() {
+        customerOrders();
     }
 }
